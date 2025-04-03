86db57b08256c53db9f46865f5e5555d5c919e6c
YossefHussein <youssef.hussein.business@gmail.com>
2025-04-03 07:29:03 +0200

    fix some problem , adding tech used screen , remove windows version

---
+++ lib/core/ads_helper.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class AdHelper {
+  static String? get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3816989531658757/9318730399';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-3816989531658757/9278421695';
+    } else {
+      throw UnsupportedError('Unsupported platform for banner ads');
+    }
+  }
+
+  static String get interstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3816989531658757/9621982720';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-3816989531658757/2338389075';
+    } else {
+      throw UnsupportedError('Unsupported platform for interstitial ads');
+    }
+  }
+
+  static String? get rewardedAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3816989531658757/5678088033';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-3816989531658757/6432766509';
+    } else {
+      throw UnsupportedError('Unsupported platform for rewarded ads');
+    }
+  }
+
+  static final BannerAdListener bannerListener = BannerAdListener(
+    onAdLoaded: (ad) => debugPrint('Ad Loaded'),
+    onAdFailedToLoad: (ad, error) {
+      ad.dispose();
+      debugPrint('Ad failed to load $error');
+    },
+    onAdClosed: (ad) => debugPrint('Ad closed'),
+  );
+}
